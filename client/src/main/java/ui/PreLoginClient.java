94461226d7f78d14678b353ee311f56728efe032
Eric Upton-Rowley <rowleyup@gmail.com>
2024-08-03 12:45:30 -0600

    Wrote PreLoginClient functions

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,49 +1,51 @@
 package ui;
 
+import model.AuthData;
+import model.UserData;
 import server.ResponseException;
 import server.ServerFacade;
-import java.util.Arrays;
+import static ui.EscapeSequences.*;
 
 public class PreLoginClient implements ChessClient{
     private final String serverUrl;
     private final ServerFacade server;
+    private final ChessClient nextClient;
 
-    public PreLoginClient(String serverUrl) {
+    public PreLoginClient(String serverUrl, ChessClient nextClient) {
         this.serverUrl = serverUrl;
         server = new ServerFacade(serverUrl);
+        this.nextClient = nextClient;
     }
 
     @Override
     public String eval(String input) {
-        try {
-            var inputs = input.toLowerCase().split(" ");
-            var command = (inputs.length > 0) ? inputs[0] : "help";
+        var inputs = input.toLowerCase().split(" ");
+        var command = (inputs.length > 0) ? inputs[0] : "help";
 
-            return switch (command) {
-                case "quit" -> quit();
-                case "login" -> login();
-                case "register" -> register();
-                default -> help();
-            };
-        } catch (ResponseException e) {
-            return e.getMessage();
-        }
+        return switch (command) {
+            case "quit" -> "quit";
+            case "login" -> "login";
+            case "register" -> "register";
+            default -> help();
+        };
     }
 
     @Override
     public String help() {
-        return "";
+        String message = SET_TEXT_COLOR_BLUE + "\tquit " + SET_TEXT_COLOR_LIGHT_GREY + "- exit program\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\tregister " + SET_TEXT_COLOR_LIGHT_GREY + "- create and account\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\tlogin " + SET_TEXT_COLOR_LIGHT_GREY + "- an existing user\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\thelp " + SET_TEXT_COLOR_LIGHT_GREY + "- display this message again\n";
+        return message;
     }
 
-    public String quit() {
-        return "";
+    public void login(String username, String password) throws ResponseException {
+        AuthData auth = server.login(new UserData(username, password, null));
+        nextClient.setAuth(auth);
     }
 
-    public String login() {
-        return "";
-    }
-
-    public String register() {
-        return "";
+    public void register(String username, String password, String email) throws ResponseException {
+        AuthData auth = server.register(new UserData(username, password, email));
+        nextClient.setAuth(auth);
     }
 }
--- client/src/main/java/ui/PreLoginRepl.java
+++ client/src/main/java/ui/PreLoginRepl.java
@@ -5,9 +5,11 @@ import static ui.EscapeSequences.*;
 
 public class PreLoginRepl implements Repl{
     private final ChessClient client;
+    private final Repl postLogin;
 
     public PreLoginRepl(String url) {
-        client = new PreLoginClient(url);
+        postLogin = new PostLoginRepl(url);
+        client = new PreLoginClient(url, postLogin.client());
     }
 
     @Override
@@ -21,10 +23,22 @@ public class PreLoginRepl implements Repl{
         while (!result.equals("quit")) {
             printPrompt();
             String inLine = scanner.nextLine();
+            String response;
 
             try {
                 result = client.eval(inLine);
-                System.out.print(SET_TEXT_COLOR_GREEN + result);
+
+                switch (result) {
+                    case "quit" -> response = "";
+                    case "login" -> response = loginPrompt();
+                    case "register" -> response = registerPrompt();
+                    default -> response = result;
+                }
+                System.out.print(SET_TEXT_COLOR_GREEN + response);
+
+                if (result.equals("login") || result.equals("register")) {
+                    postLogin.run();
+                }
             } catch (Throwable e) {
                 String message = e.toString();
                 System.out.print(SET_TEXT_BLINKING + SET_TEXT_BOLD + SET_TEXT_COLOR_RED + message + RESET_TEXT_BLINKING);
@@ -34,6 +48,10 @@ public class PreLoginRepl implements Repl{
 
     @Override
     public void printPrompt() {
-        System.out.print("\n" + RESET_TEXT_COLOR + "[pre-login] >>> " + SET_TEXT_COLOR_BLUE);
+        System.out.print("\n" + RESET_TEXT_COLOR + "[logged out] >>> " + SET_TEXT_COLOR_BLUE);
     }
+
+    private String loginPrompt() {}
+
+    private String registerPrompt() {}
 }
