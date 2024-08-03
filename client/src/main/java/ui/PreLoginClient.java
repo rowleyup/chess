28e0e3dd0595268de2cfe34de0dc0490cd078008
Eric Upton-Rowley <rowleyup@gmail.com>
2024-08-03 12:12:32 -0600

    Wrote eval function in PreLoginClient

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,19 +1,49 @@
 package ui;
 
+import server.ResponseException;
+import server.ServerFacade;
+import java.util.Arrays;
+
 public class PreLoginClient implements ChessClient{
     private final String serverUrl;
+    private final ServerFacade server;
 
     public PreLoginClient(String serverUrl) {
         this.serverUrl = serverUrl;
+        server = new ServerFacade(serverUrl);
     }
 
     @Override
     public String eval(String input) {
-        return "";
+        try {
+            var inputs = input.toLowerCase().split(" ");
+            var command = (inputs.length > 0) ? inputs[0] : "help";
+
+            return switch (command) {
+                case "quit" -> quit();
+                case "login" -> login();
+                case "register" -> register();
+                default -> help();
+            };
+        } catch (ResponseException e) {
+            return e.getMessage();
+        }
     }
 
     @Override
     public String help() {
+        return "";
+    }
+
+    public String quit() {
+        return "";
+    }
+
+    public String login() {
+        return "";
+    }
+
+    public String register() {
         return "";
     }
 }
