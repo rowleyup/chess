a389e3743d94ccb48fe4d2d985eea57cf92b7114
Eric Upton-Rowley <rowleyup@gmail.com>
2024-08-05 17:18:00 -0600

    Finished Main method

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import chess.*;
+import ui.PreLoginRepl;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        String url = "http://localhost:8080/";
+        new PreLoginRepl(url).run();
     }
 }
