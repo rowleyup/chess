6b61487e4c041b6b5b4dbeee7ac3743433837dfe
Eric Upton-Rowley <rowleyup@gmail.com>
2024-08-05 17:21:20 -0600

    Fixed startup message

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,9 @@
-import chess.*;
 import ui.PreLoginRepl;
+import static ui.EscapeSequences.WHITE_PAWN;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        System.out.println("♕ 240 Chess Client: " + WHITE_PAWN);
 
         String url = "http://localhost:8080/";
         new PreLoginRepl(url).run();
