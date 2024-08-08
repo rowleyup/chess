2363bf4109705d2eb319e46697a4781ce57832bf
Eric Upton-Rowley <rowleyup@gmail.com>
2024-08-08 13:56:29 -0600

    Initialized in-game client methods

--- client/src/main/java/ui/InGameClient.java
+++ client/src/main/java/ui/InGameClient.java
@@ -26,10 +26,26 @@ public class InGameClient {
     }
 
     public String help() {
-        String message = SET_TEXT_COLOR_BLUE + "\n\tquit " + SET_TEXT_COLOR_LIGHT_GREY + "- leave game\n";
-        message = message + SET_TEXT_COLOR_BLUE + "\twhite " + SET_TEXT_COLOR_LIGHT_GREY + "- print chess board from white perspective\n";
-        message = message + SET_TEXT_COLOR_BLUE + "\tblack " + SET_TEXT_COLOR_LIGHT_GREY + "- print chess board from black perspective\n";
+        String message = SET_TEXT_COLOR_BLUE + "\n\tredraw " + SET_TEXT_COLOR_LIGHT_GREY + "- redraw chess board\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\tmove " + SET_TEXT_COLOR_LIGHT_GREY + "- move a chess piece\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\thighlight " + SET_TEXT_COLOR_LIGHT_GREY + "- highlight legal moves for a piece\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\tresign " + SET_TEXT_COLOR_LIGHT_GREY + "- resign from the game\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\tleave " + SET_TEXT_COLOR_LIGHT_GREY + "- leave game\n";
         message = message + SET_TEXT_COLOR_BLUE + "\thelp " + SET_TEXT_COLOR_LIGHT_GREY + "- display this message again\n";
         return message;
     }
+
+    public String overHelp() {
+        String message = SET_TEXT_COLOR_BLUE + "\n\tredraw " + SET_TEXT_COLOR_LIGHT_GREY + "- redraw chess board\n";
+        message = message + SET_TEXT_COLOR_LIGHT_GREY + "\tmove - move a chess piece\n";
+        message = message + SET_TEXT_COLOR_LIGHT_GREY + "\thighlight - highlight legal moves for a piece\n";
+        message = message + SET_TEXT_COLOR_LIGHT_GREY + "\tresign - resign from the game\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\tleave " + SET_TEXT_COLOR_LIGHT_GREY + "- leave game\n";
+        message = message + SET_TEXT_COLOR_BLUE + "\thelp " + SET_TEXT_COLOR_LIGHT_GREY + "- display this message again\n";
+        return message;
+    }
+
+    public String leave() {}
+
+    public String move() {}
 }
